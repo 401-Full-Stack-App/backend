79e59464a5361805e7848a12a881bbdca4fd7097
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-09-30 16:56:55 -0700

    begin supergoose testing

---
+++ src/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// 3rd party resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// exporting server and start
+module.exports = {
+  server: app,
+  /** start server on specified port
+   * @param port {number} (defaults to process.env.PORT)
+   */
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  },
+};
